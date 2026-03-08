1370321d4ddf77985e8bbb4c65ff7a8748610123
SAJITHRAN <sajithran37@gmail.com>
2026-03-08 19:39:42 +0530

    Update sjt.c

--- sjt.c
+++ sjt.c
@@ -1,71 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Process{
-    char pid[10];
-    int at, bt, ct, wt, tat;
-    int finished;
+struct Process {
+	char pid[10];
+	int at, bt, ct, wt, tat;
+	int finished;
 };
 
-int main(){
+int main() {
 
-    int n;
-    scanf("%d",&n);
+	int n;
+	scanf("%d",&n);
 
-    struct Process proc[100];
+	struct Process proc[100];
 
-    for(int i=0;i<n;i++){
-        scanf("%s %d %d",proc[i].pid,&proc[i].at,&proc[i].bt);
-        proc[i].finished=0;
-    }
+	for(int i=0; i<n; i++) {
+		scanf("%s %d %d",proc[i].pid,&proc[i].at,&proc[i].bt);
+		proc[i].finished=0;
+	}
 
-    int currentTime=0, doneCount=0;
+	int currentTime=0, doneCount=0;
 
-    while(doneCount<n){
+	while(doneCount<n) {
 
-        int index=-1;
-        int minBT=9999;
+		int index=-1;
+		int minBT=9999;
 
-        for(int i=0;i<n;i++){
-            if(proc[i].finished==0 && proc[i].at<=currentTime){
-                if(proc[i].bt<minBT){
-                    minBT=proc[i].bt;
-                    index=i;
-                }
-            }
-        }
+		for(int i=0; i<n; i++) {
+			if(proc[i].finished==0 && proc[i].at<=currentTime) {
+				if(proc[i].bt<minBT) {
+					minBT=proc[i].bt;
+					index=i;
+				}
+			}
+		}
 
-        if(index==-1){
-            currentTime++;
-        }
-        else{
-            currentTime+=proc[index].bt;
+		if(index==-1) {
+			currentTime++;
+		}
+		else {
+			currentTime+=proc[index].bt;
 
-            proc[index].ct=currentTime;
-            proc[index].tat=proc[index].ct-proc[index].at;
-            proc[index].wt=proc[index].tat-proc[index].bt;
+			proc[index].ct=currentTime;
+			proc[index].tat=proc[index].ct-proc[index].at;
+			proc[index].wt=proc[index].tat-proc[index].bt;
 
-            proc[index].finished=1;
-            doneCount++;
-        }
-    }
+			proc[index].finished=1;
+			doneCount++;
+		}
+	}
 
-    float totalWT=0,totalTAT=0;
+	float totalWT=0,totalTAT=0;
 
-    printf("Waiting Time:\n");
-    for(int i=0;i<n;i++){
-        printf("%s %d\n",proc[i].pid,proc[i].wt);
-        totalWT+=proc[i].wt;
-    }
+	printf("Waiting Time:\n");
+	for(int i=0; i<n; i++) {
+		printf("%s %d\n",proc[i].pid,proc[i].wt);
+		totalWT+=proc[i].wt;
+	}
 
-    printf("Turnaround Time:\n");
-    for(int i=0;i<n;i++){
-        printf("%s %d\n",proc[i].pid,proc[i].tat);
-        totalTAT+=proc[i].tat;
-    }
+	printf("Turnaround Time:\n");
+	for(int i=0; i<n; i++) {
+		printf("%s %d\n",proc[i].pid,proc[i].tat);
+		totalTAT+=proc[i].tat;
+	}
 
-    printf("Average Waiting Time: %.2f\n",totalWT/n);
-    printf("Average Turnaround Time: %.2f\n",totalTAT/n);
+	printf("Average Waiting Time: %.2f\n",totalWT/n);
+	printf("Average Turnaround Time: %.2f\n",totalTAT/n);
 
-    return 0;
+	return 0;
 }
+
