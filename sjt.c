1c344f25da0acbbbcb880f18d5ef75b509528e77
SAJITHRAN <sajithran37@gmail.com>
2026-03-08 19:45:05 +0530

    Update sjt.c

--- sjt.c
+++ sjt.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 struct Process {
 	char pid[10];
 	int at, bt, ct, wt, tat;
@@ -8,24 +7,17 @@ struct Process {
 };
 
 int main() {
-
 	int n;
-	scanf("%d",&n);
-
+	scanf("%d",&n)
 	struct Process proc[100];
-
 	for(int i=0; i<n; i++) {
 		scanf("%s %d %d",proc[i].pid,&proc[i].at,&proc[i].bt);
 		proc[i].finished=0;
 	}
-
 	int currentTime=0, doneCount=0;
-
 	while(doneCount<n) {
-
 		int index=-1;
 		int minBT=9999;
-
 		for(int i=0; i<n; i++) {
 			if(proc[i].finished==0 && proc[i].at<=currentTime) {
 				if(proc[i].bt<minBT) {
@@ -34,24 +26,19 @@ int main() {
 				}
 			}
 		}
-
 		if(index==-1) {
 			currentTime++;
 		}
 		else {
 			currentTime+=proc[index].bt;
-
 			proc[index].ct=currentTime;
 			proc[index].tat=proc[index].ct-proc[index].at;
 			proc[index].wt=proc[index].tat-proc[index].bt;
-
 			proc[index].finished=1;
 			doneCount++;
 		}
 	}
-
 	float totalWT=0,totalTAT=0;
-
 	printf("Waiting Time:\n");
 	for(int i=0; i<n; i++) {
 		printf("%s %d\n",proc[i].pid,proc[i].wt);
@@ -63,10 +50,7 @@ int main() {
 		printf("%s %d\n",proc[i].pid,proc[i].tat);
 		totalTAT+=proc[i].tat;
 	}
-
 	printf("Average Waiting Time: %.2f\n",totalWT/n);
 	printf("Average Turnaround Time: %.2f\n",totalTAT/n);
-
 	return 0;
 }
-
